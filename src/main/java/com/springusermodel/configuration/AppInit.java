8033f5c5271650b33d40cc0948ef3575f765de86
Hai <thanhhaitran1203@gmail.com>
2024-02-29 15:56:07 +0700

    done

---
+++ src/main/java/com/springusermodel/configuration/AppInit.java
@@ -0,0 +1,17 @@
+package com.springusermodel.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
